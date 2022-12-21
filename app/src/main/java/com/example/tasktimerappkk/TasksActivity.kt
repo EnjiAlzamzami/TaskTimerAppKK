56892139f94a3316130dec26898c830741ce74fa
SulimanBinMo <sbinmohammed_1@icloud.com>
2022-12-21 14:52:00 +0300

    created add task page as an activity, homepage(Task list) as an activity, login page item_row(task), added icons in drawable,

---
+++ app/src/main/java/com/example/tasktimerappkk/TasksActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tasktimerappkk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TasksActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tasks)
+    }
+}
